f7e5b4dde00209c166695c4863418b25ab43a28b
ulapkus <ulapkus@gmail.com>
2023-09-29 14:44:02 -0500

    added ability to change table cell color when clicked

--- app/components/chart.js
+++ app/components/chart.js
@@ -6,6 +6,21 @@ export const Context = React.createContext();
 export default function Chart() {
   const [random, setRandom] = useState([]);
 
+  const location = (event) => {
+    const clickedCell = event.target;
+    const rowIndex = clickedCell.parentElement.rowIndex;
+    const cellIndex = clickedCell.cellIndex;
+
+    if (event.target.tagName === "TD") {
+      console.log(rowIndex);
+    }
+    for (let i = 0; i <= 6; i++) {
+      if (rowIndex === 1 && cellIndex === i) {
+        document.getElementsByTagName("TD")[i].style.backgroundColor = "red";
+      }
+    }
+  };
+
   return (
     <div className="background">
       {/* <h1>My Habit Tracker</h1> */}
@@ -14,26 +29,42 @@ export default function Chart() {
           <Context.Provider value={[random, setRandom]}>
             <Modal />
           </Context.Provider>
-
-          <table>
-            <tbody>
-              <tr>
-                <th>Habit</th>
-                <th>Day 1</th>
-                <th>Day 2</th>
-                <th>Day 3</th>
-                <th>Day 4</th>
-                <th>Day 5</th>
-                <th>Day 6</th>
-                <th>Day 7</th>
-              </tr>
-            </tbody>
+          <section className="habits">
+            <h3 className="tablebody">Habit</h3>
             {random.map((item, index) => (
-              <tbody key={index}>
+              <p key={index} className="tablebody">
+                {item}
+              </p>
+            ))}
+          </section>
+
+          <section className="days-boxes">
+            <table>
+              <thead>
                 <tr>
-                  <td>{item}</td>
+                  <th>Day 1</th>
+                  <th>Day 2</th>
+                  <th>Day 3</th>
+                  <th>Day 4</th>
+                  <th>Day 5</th>
+                  <th>Day 6</th>
+                  <th>Day 7</th>
+                </tr>
+              </thead>
+
+              <tbody className="habit-rows" id="mytable" onClick={location}>
+                <tr>
+                  <td>row 1 box 1</td>
+                  <td>row 1 box 2</td>
                   <td>box</td>
                   <td>box</td>
+                  <td>box</td>
+                  <td>box</td>
+                  <td>box</td>
+                </tr>
+                <tr>
+                  <td>row 2 box 1</td>
+                  <td>row 2 box 2</td>
                   <td>box</td>
                   <td>box</td>
                   <td>box</td>
@@ -41,14 +72,28 @@ export default function Chart() {
                   <td>box</td>
                 </tr>
               </tbody>
-            ))}
-
-            <tbody>
-              <tr></tr>
-            </tbody>
-          </table>
+            </table>
+          </section>
         </section>
       </div>
     </div>
   );
 }
+
+// OLD:
+// {random.map((item, index) => (
+//   <tbody key={index} className="tablebody">
+//     <tr>
+//       {/* <td className="clickbox" >{item}</td> */}
+//       <td id="clickboxx" className="clickbox" onClick={location}>box</td>
+//       {/* <td id="clickboxx" className="clickbox" onClick={location}>box</td> */}
+
+//       {/* <td>box</td>
+//        <td>box</td>
+//       <td>box</td>
+//       <td>box</td>
+//       <td>box</td>
+//       <td>box</td> */}
+//     </tr>
+//   </tbody>
+// ))}
--- app/components/questions.js
+++ app/components/questions.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import { Context } from "./chart";
 
-export default function Question() {
+export default function Child() {
   const [random, setRandom] = useContext(Context);
 
   const createNewElement = () => {
