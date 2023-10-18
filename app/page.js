871448d9520b6cb154fc972a60ca0c3cc8d6fedb
ulapkus <ulapkus@gmail.com>
2023-10-18 18:20:59 -0500

    test three

--- app/page.js
+++ app/page.js
@@ -1,21 +1,21 @@
 
 
-export default async function Home() {
-  return <main>Hello</main>
-}
-
-// "use client";
-
-// import Tabs from "./Tabs";
-// import Footer from "./components/footer";
-
-// export default function Home() {
-//   return (
-//     <div className="main">
-//       <Tabs />
-//       <div className="main-footer">
-//         <Footer />
-//       </div>
-//     </div>
-//   );
+// export default async function Home() {
+//   return <main>Hello</main>
 // }
+
+"use client";
+
+import Tabs from "./Tabs";
+import Footer from "./components/footer";
+
+export default function Home() {
+  return (
+    <div className="main">
+      <Tabs />
+      <div className="main-footer">
+        <Footer />
+      </div>
+    </div>
+  );
+}
