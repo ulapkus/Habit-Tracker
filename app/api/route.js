8219f08b3a206300562650f45fbc689668764293
ulapkus <ulapkus@gmail.com>
2023-11-09 22:35:55 +0700

    firebase portion commented out

--- app/api/route.js
+++ app/api/route.js
@@ -1,16 +1,16 @@
-import { initializeApp } from 'firebase-admin/app';
+// import { initializeApp } from 'firebase-admin/app';
 
-initializeApp({
-    credential: applicationDefault(),
-    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
-});
+// initializeApp({
+//     credential: applicationDefault(),
+//     databaseURL: 'https://<DATABASE_NAME>.firebaseio.com'
+// });
 
-export async function POST(request) {
-  // res = {username: "ula", password: "password"}
-  const res = await request.json();
-  const username = res.username;
-  const password = res.password;
-  return Response.json({ yourusername: username, yourpassword: password });
-}
+// export async function POST(request) {
+//   // res = {username: "ula", password: "password"}
+//   const res = await request.json();
+//   const username = res.username;
+//   const password = res.password;
+//   return Response.json({ yourusername: username, yourpassword: password });
+// }
 
 
