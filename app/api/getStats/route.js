081d8de09b433d1d2c239dabe121e2577e012c1e
ulapkus <ulapkus@gmail.com>
2024-01-12 23:14:01 +0800

    updates stats page

---
+++ app/api/getStats/route.js
@@ -0,0 +1,15 @@
+import Connect from "../../../utils/db";
+import Users from "../../../models/User";
+import { NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+
+export async function GET() {
+    await Connect();
+    const session = await getServerSession();
+    const query = { email: session.user.email };
+
+    const data = await Users.find(query, { _id: 0, createdAt: 1 });
+  
+    return NextResponse.json({ data }, { status: 200 });
+  }
+  
--- app/stats/page.js
+++ app/stats/page.js
@@ -1,42 +1,45 @@
 "use client";
 
-import React, { useEffect, useState, createContext } from "react";
+import React, { useEffect, useState } from "react";
+
 import { differenceInDays } from "date-fns";
 import { useSession } from "next-auth/react";
 
-// export const StartDateContext = React.createContext();
-export const NameContext = createContext([[], () => {}]);
-
-export const StartDateContext = createContext([[], () => {}]);
-
 export default function Stats() {
-  const [nameOfUser, setNameOfUser] = useState("Ula");
-  const [startDate, setStartDate] = useState(["2023-10-12"]);
+  const [dateCreated, setDateCreated] = useState();
   const { data: session } = useSession();
+  const daysDifference = differenceInDays(new Date(), new Date(dateCreated));
 
-  const daysDifference = differenceInDays(new Date(), new Date(startDate));
+  async function getStartDate() {
+    try {
+      const res = await fetch("/api/getStats");
 
-  const begin = () => {
-    setStartDate("2023-10-31");
-  };
+      if (!res.ok) {
+        throw new Error("Error fetching users", error.message);
+      }
+      const modalResponse = await res.json();
+      const updatedData = Object.values(modalResponse.data[0])[0];
+      setDateCreated(updatedData);
+    } catch (error) {
+      console.log("Error fetching current user", error.message);
+    }
+  }
+
+  useEffect(() => {
+    getStartDate();
+  }, []);
 
   return (
     <div>
-      <div>
-        {/* <NameContext.Provider value={[]}>
-          <StartDateContext.Provider value={[]}>
-            <Signinn />
-          </StartDateContext.Provider>
-        </NameContext.Provider> */}
-      </div>
       <div className="stats-greeting">
         <h5>HELLO,&nbsp;</h5>
-        {!session ? null : <div className="stats-user"> {session.user?.name}!</div>}
+        {!session ? null : (
+          <div className="stats-user"> {session.user?.name}!</div>
+        )}
       </div>
       <p className="days-tracked">
         You have been tracking your habits for {daysDifference} days
       </p>
-      {/* <button onClick={begin}>press</button> */}
     </div>
   );
 }
