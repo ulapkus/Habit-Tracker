7edfd30f01e90420b32c43d728e7c9305a6e23ff
ulapkus <ulapkus@gmail.com>
2024-02-11 22:18:53 +0800

    added screenshots for readme

--- app/api/updateState/route.js
+++ app/api/updateState/route.js
@@ -1,34 +1,33 @@
-import Connect from "../../../utils/db";
-import Users from "../../../models/User";
-import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
+// import Connect from "../../../utils/db";
+// import Users from "../../../models/User";
+// import { NextResponse } from "next/server";
+// import { getServerSession } from "next-auth";
 
-export async function POST(request) {
-  try {
-    const { daysData, colorData, habitData } = await request.json();
-    await Connect();
-    const session = await getServerSession();
-    const thisuser = session.user.email;
-    const filter = { email: thisuser };
-    const result = await Users.updateOne(filter, {
-      $set: { days: daysData, colors: colorData},
-      $addToSet: { habits: { $each: habitData } },
-    });
+// export async function POST(request) {
+//   try {
+//     const { daysData, colorData, habitData } = await request.json();
+//     await Connect();
+//     const session = await getServerSession();
+//     const thisuser = session.user.email;
+//     const filter = { email: thisuser };
+//     const result = await Users.updateOne(filter, {
+//       $set: { days: daysData, colors: colorData},
+//       $addToSet: { habits: { $each: habitData } },
+//     });
 
-    if (result.modifiedCount === 1) {
-      return NextResponse.json(
-        { message: "User updated successfully" },
-        { status: 200 }
-      );
-    } else {
-      return NextResponse.json({ message: "User not found" }, { status: 404 });
-    }
-  } catch (error) {
-    console.error("Error updating user:", error);
-    return NextResponse.json(
-      { message: "Error updating user", error },
-      { status: 500 }
-    );
-  }
-}
-
+//     if (result.modifiedCount === 1) {
+//       return NextResponse.json(
+//         { message: "User updated successfully" },
+//         { status: 200 }
+//       );
+//     } else {
+//       return NextResponse.json({ message: "User not found" }, { status: 404 });
+//     }
+//   } catch (error) {
+//     console.error("Error updating user:", error);
+//     return NextResponse.json(
+//       { message: "Error updating user", error },
+//       { status: 500 }
+//     );
+//   }
+// }
